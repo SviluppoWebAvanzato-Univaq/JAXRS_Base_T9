83c484c12a64f82a94cb0f38f1a4ead00bd2fad6
Giuseppe Della Penna <giuseppe.dellapenna@univaq.it>
2022-05-21 11:31:13 +0200

    Initial commit

---
+++ src/main/java/org/univaq/swa/framework/base/RESTWebApplicationException.java
@@ -0,0 +1,25 @@
+package org.univaq.swa.framework.base;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+public class RESTWebApplicationException extends WebApplicationException {
+
+    public RESTWebApplicationException() {
+        super(Response.serverError().build());
+    }
+
+    public RESTWebApplicationException(String message) {
+        super(Response.serverError()
+                .entity(message)
+                .type("text/plain")
+                .build());
+    }
+
+    public RESTWebApplicationException(int status, String message) {
+        super(Response.status(status)
+                .entity(message)
+                .type("text/plain")
+                .build());
+    }
+}
